5344b85ee7b1b1c7f43e306d8308d7ca0628b262
Raga Subekti <rfilmriot@gmail.com>
2019-04-08 16:24:24 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const onCalculate = () => {
+  const operator = document.getElementById("operator").value;
+  const first = parseInt(document.getElementById("first").value);
+  const second = parseInt(document.getElementById("second").value);
+
+  let result = 0;
+
+  switch (operator) {
+    case "-":
+      result = first - second;
+      break;
+    case "/":
+      result = first / second;
+      break;
+    case "x":
+      result = first * second;
+      break;
+    default:
+      result = first + second;
+  }
+
+  document.getElementById("result").innerHTML = result;
+};
+
+const calculateShape = () => {
+  const sisi = parseInt(document.getElementById("sisi").value);
+  const result = sisi ** 2;
+
+  document.getElementById("luas-result").innerHTML = result;
+};
